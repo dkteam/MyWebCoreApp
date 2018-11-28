a4c509612b9ce61bed811358153e585e3dfeda98
Pham Dang Khoa <mr.pdkhoa@gmail.com>
2018-11-28 15:53:42 +0700

    First commit

--- MyWebCoreApp/Controllers/HomeController.cs
+++ MyWebCoreApp/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace MyWebCoreApp.Controllers
             return View();
         }
 
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
